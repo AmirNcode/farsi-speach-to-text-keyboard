ea377f5080710e2aa70d197df7355812fef0eabd
amir-nrfkn <amir.dev21@gmail.com>
2026-06-17 12:39:59 -0400

    feat(ios): scaffold app + keyboard extension targets

---
+++ ios/FarsiVoiceKeyboard/FarsiVoiceKeyboard/FarsiVoiceKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FarsiVoiceKeyboardApp.swift
+//  FarsiVoiceKeyboard
+//
+//  Created by Amir Noorafkan on 2026-06-17.
+//
+
+import SwiftUI
+
+@main
+struct FarsiVoiceKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
